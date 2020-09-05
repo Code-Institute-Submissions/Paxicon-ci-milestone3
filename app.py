b1c198cb7613868fe02f1e2e1d1bba7fd41fa977
Patrik <paxicon84@gmail.com>
2020-09-05 23:33:46 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=5000,
+            debug=True)
