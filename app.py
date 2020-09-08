6c302ab2536fecd7e0a1d3a9a319ac3cbfe6cd2a
Patrik <paxicon84@gmail.com>
2020-09-08 19:06:59 +0200

    finished member profile login-logout functions

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration succesful!")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -76,6 +77,8 @@ def login():
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 flash("Username and/or password incorrect.")
                 return redirect(url_for("login"))
@@ -86,10 +89,23 @@ def login():
 
     return render_template("login.html")
 
+# Profile page
 
-@app.route('/profile', methods=["GET", "POST"])
-def profile():
-    return render_template("profile.html")
+
+@app.route('/profile/<username>', methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=session["user"])
+
+# Logout function + redirect
+
+
+@ app.route('/logout')
+def logout():
+    flash("You have been logged out!")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
