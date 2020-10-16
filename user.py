cac9eb4d12497730d5feed57a600ca1d3dd9706a
Patrik <paxicon84@gmail.com>
2020-10-16 19:24:34 +0200

    Commented, cleaned user.py

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 # This file contains setup of the User class for the flask-login extension.
+
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_mongoengine import MongoEngine, Document
 from flask_login import UserMixin
@@ -14,18 +15,10 @@ class User(UserMixin, db.DynamicDocument):
     meta = {'collection': 'User'}
     display_name = db.StringField(max_length=50)
     email = db.StringField(max_length=50)
-    password = db.StringField()
+    password = db.StringField(min_length=8, max_length=20)
     # A required attribute from flask-login to ensure the login.required decorators operate functionally. As auth here is very simple, all users are considered authenticated if they pass login.
     is_authenticated = True
     # As required by flask-login. For our purposes, all users are active users.
     is_active = True
     # As required by flask-login. For our purposes, there are no anonymous users.
     is_anonymous = False
-    # Method for returning a hashed, secure password.
-
-    def set_pw(self, password):
-        return set_pw
-
-    # Method for validating the password of a user.
-    def check_pw(self, password):
-        return check_password_hash(self['password'], password)
