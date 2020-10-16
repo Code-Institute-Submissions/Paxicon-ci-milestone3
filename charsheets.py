56be45513a566c642c015ac819b61c0d2ec27436
Patrik <paxicon84@gmail.com>
2020-10-16 19:38:44 +0200

    Cleaned, commented charsheets.py

--- charsheets.py
+++ charsheets.py
@@ -1,5 +1,7 @@
 # This file contains the models for the document object, CharacterSheets for Dungeons & Dragons. It is kept separated in this file because of separations
 # of concerns, as this is a complex data-object with multiple nested arrays and embedded dicts, not to mention subfields for rendering, it would quickly make app.py unreadable.
+# The following list of imports various parts of the Flask-MongoEngine and Flask-WTF extensions to make handling database-schema planning and form validation easier,
+# as well as some widgets for passing to input fields.
 
 from flask_wtf import *
 from user import User
@@ -12,134 +14,20 @@ from wtforms import *
 from flask_wtf.form import *
 from wtforms.fields.html5 import *
 from wtforms.widgets.html5 import *
+
 db = MongoEngine()
 
-# 'Attributes' are a dict of ints that can range from 1 to 20. All characters have a set of Attributes within this range. This will be passed
-# to a jQuery script that will then calculate required values based on these database posts. As those values can be modified so frequently
-# and are calculated from the base-attribute value, it is preferable to defer the modifier to the FrontEnd to save on r/w operations.
-
-
-class CharAttributes(db.EmbeddedDocument):
-    strength = db.IntField(min_value=1, max_value=20)
-    dexterity = db.IntField(min_value=1, max_value=20)
-    constitution = db.IntField(min_value=1, max_value=20)
-    intelligence = db.IntField(min_value=1, max_value=20)
-    wisdom = db.IntField(min_value=1, max_value=20)
-    charisma = db.IntField(min_value=1, max_value=20)
-
-# Saves is a list of booleans, represented in HTML as checkboxes. A save in the context of the rules is either true or false. If true, it adds a bonus based on
-# proficiency modifier. As the modifier is calculated on the front-end, the database need only pass True or False to the front-end.
-
-
-class Saves(db.EmbeddedDocument):
-    StrSave = db.BooleanField()
-    DexSave = db.BooleanField()
-    ConSave = db.BooleanField()
-    IntSave = db.BooleanField()
-    WisSave = db.BooleanField()
-    ChaSave = db.BooleanField()
-
-# 'Skills' is a boolean list, represented in HTML as checkboxes. All characters use the list of skills, so it is a separate embedded document like attributes.
-# The user will mark a number of skills as 'proficient' and when the data is passed to the front-end, values will be generated using jQuery
-# to accurately calculate the exact numeric value based on proficiency. As this number can value based on modifiers, it is not worth writing exact
-# numbers into the DB there, you can get accurate data for our purposes knowing just if it is proficient or not.
-
-
-class Skills(db.EmbeddedDocument):
-    Athletics = db.BooleanField()
-    Acrobatics = db.BooleanField()
-    Sleight = db.BooleanField()
-    Stealth = db.BooleanField()
-    Arcana = db.BooleanField()
-    History = db.BooleanField()
-    Investigation = db.BooleanField()
-    Nature = db.BooleanField()
-    Religion = db.BooleanField()
-    AnimalHandling = db.BooleanField()
-    Insight = db.BooleanField()
-    Medicine = db.BooleanField()
-    Perception = db.BooleanField()
-    Survival = db.BooleanField()
-    Deception = db.BooleanField()
-    Intimidation = db.BooleanField()
-    Performance = db.BooleanField()
-    Persuasion = db.BooleanField()
-
-# 'Armor' at its core represents an integer that theoretically has neither a maximum nor minimum value. However, numerous
-# factors can alter armor-class and it is feasible for a user to want to save multiple 'Armor' objects to represent
-# different equipment in a game. Therefore, Armor will be accessible through a EmbeddedDocument
-# Field of multiple armor-items in
-# the DB, which user can add, update or remove from at will. Some of the more basic rules are incorporated for ease-of-use
-# in the form of the required boolean-fields.
-
-
-class Armor(db.EmbeddedDocument):
-    Name = db.StringField()
-    CharDescription = db.StringField()
-    ACValue = db.IntField()
-    HeavyArmor = db.BooleanField()
-    MediumArmor = db.BooleanField()
-    LightArmor = db.BooleanField()
-    Shield = db.BooleanField()
-
-
-# 'Attacks', much like Armor, are a class of what is essentially a collection of ints. An attack roll is a randomized number
-# adding the modifier, the modifier being calculated client-side on the front-end, the object needs only save name, description, dice-type
-# and defer all other math operations to the JavaScript component.
-
-
-class Attacks(db.EmbeddedDocument):
-    Name = db.StringField()
-    Description = db.StringField()
-    DmgDie = db.IntField()
-
-
-class AttackObjs(db.EmbeddedDocument):
-    AttacksList = db.EmbeddedDocumentField(Attacks)
-
-# The ClassObj contains name, description and level as well as an embedded list-item classes as "Ability", a catch-all term for character-abilities
-# that the user must specify and keep updated themselves. Proficiency is a function of level/4 +1 (Rounded up) and is thus referred to the front-end which handles
-# derived stats.
-
-
-class Abilities(db.EmbeddedDocument):
-    Name = db.StringField()
-    Description = db.StringField()
-    DieType = db.IntField()
-    Attribute = FieldList(db.EmbeddedDocumentField(CharAttributes))
-
-
-class AbilityObjs(db.EmbeddedDocument):
-    AbilityList = db.EmbeddedDocumentField(Abilities)
-
-
-class ClassObj(db.EmbeddedDocument):
-
-    HitDie = db.IntField()
-    Abilities = FieldList(db.EmbeddedDocumentField(AbilityObjs))
-    AttacksPerRound = db.IntField()
-
-
-# Finally, the char object contains StringFields for name, description, looks, an int-field for level and references to all other documents to embed.
+# This class defines the Char object, the actual document saved to the database. Due to its nature as a DynamicDocument, it does not need to be a stricter schema than this
+# and all form-entries are stored as a nested item inside the document labelled "content", the outer layer containing only "content" and "Owner" keys.
 
 
 class Char(db.DynamicDocument):
 
-    Name = db.StringField()
-    CharClass = db.StringField()
-    Subclass = db.StringField()
-    Appearance = db.StringField()
-    CharDescription = db.StringField()
-    ClassObj = FieldList(db.EmbeddedDocumentField(ClassObj))
-    AttributeList = FieldList(db.EmbeddedDocumentField(CharAttributes))
-    SavesList = FieldList(db.EmbeddedDocumentField(Saves))
-    SkillsList = FieldList(db.EmbeddedDocumentField(Skills))
-    ArmorObjList = FieldList(db.EmbeddedDocumentField(Armor))
-    AttacksList = FieldList(db.EmbeddedDocumentField(Attacks))
-    AbilityObjsList = FieldList(db.EmbeddedDocumentField(AbilityObjs))
     Owner = db.ReferenceField(User, reverse_delete_rule=2)
 
-# The following forms is what is passed to the Frontend and Jinja to register new Chars.
+# Below this polnt are the individual forms that is passed to jinja for rendering and to Flask-WTF for validation on submit.
+# All forms given the value of (Form) are contained as nested objects inside the larger CharInput() object, which renders them as part of a single form element.
+# Some classes have been commented out, because they are planned for future updates after submission.
 
 
 class CharAttributesForm(Form):
@@ -184,21 +72,25 @@ class SaveForm(Form):
     ChaSave = BooleanField('Charisma saving throw: ')
 
 
-class AbilityObjForm(Form):
-    class Meta:
-        csrf = False
-    Name = StringField('Ability name: ')
+# **** IGNORE THE FOLLOWING TWO CLASSES! ****
+# They are for a feature I plan to add post-submission and are not to be considered active part of the app at the time of submission. I simply wanted to keep the preparation I had done
+# and not have to rewrite it after my grade was finished.
+
+# class AbilityObjForm(Form):
+#    class Meta:
+#        csrf = False
+#    Name = StringField('Ability name: ')
 
 
-class AbilitiesForm(Form):
-    class Meta:
-        csrf = False
-    Name = StringField('Name of ability')
-    Description = StringField(' Describe the ability, effects, damage, DCs: ')
-    DieType = IntegerField('Enter the primary dice for this ability: ')
-    Attribute = FormField(CharAttributes, "Your character attributes: ")
-    AbilityObjList = FormField(
-        AbilityObjForm, 'Add abilities your character knows here: ')
+# class AbilitiesForm(Form):
+#    class Meta:
+#        csrf = False
+#    Name = StringField('Name of ability')
+#    Description = StringField(' Describe the ability, effects, damage, DCs: ')
+#    DieType = IntegerField('Enter the primary dice for this ability: ')
+#    Attribute = FormField(CharAttributesForm, "Your character attributes: ")
+#    AbilityObjList = FormField(
+#        AbilityObjForm, 'Add abilities your character knows here: ')
 
 
 class SkillsForm(Form):
